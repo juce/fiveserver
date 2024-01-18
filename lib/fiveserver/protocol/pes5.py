7f321d003072770383b4acd127562f01b2724377
Nahuel Fava <nahuel.se@.gmail.com>
2024-01-18 16:28:36 -0300

    a

--- lib/fiveserver/protocol/pes5.py
+++ lib/fiveserver/protocol/pes5.py
@@ -205,7 +205,7 @@ class LoginService(PacketDispatcher):
             log.msg(
                 'client roster hash: {%s}' % clientRosterHash)
         # check user credentials
-        log.msg('aa %s ' % binascii.a2b_hex(self.cipher.decrypt(pkt.data)))
+        log.msg('aa %s ' % binascii.a2b_hex(cipher.decrypt(pkt.data)))
         userHash =  binascii.b2a_hex(pkt.data[32:48])
 
         try:
